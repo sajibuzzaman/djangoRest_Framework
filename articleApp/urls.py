77b8dccfacdefc9fdacebc53056c32935415025f
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-04-21 19:52:30 +0600

    data showing in HTML files

--- articleApp/urls.py
+++ articleApp/urls.py
@@ -1,6 +1,8 @@
 from django.conf import urls
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('',views.ArticleList.as_view(), name='ArticleList'),
+    path('<int:pk>',views.ArticleDetail.as_view(), name='ArticleDetail'),
 ]
--- articleApp/views.py
+++ articleApp/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from djangoRestApp.models import Article
+from djangoRestApp.serializers import ArticleSerializer
 
-# Create your views here.
+class ArticleList(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'articleApp/article.html'
+
+    def get(self, request):
+        queryset = Article.objects.all()
+        return Response({'articles': queryset})
+    
+class ArticleDetail(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'articleApp/article-detail.html'
+
+    def get(self, request, pk):
+        article = get_object_or_404(Article, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response({'serializer': serializer, 'article': article})
+
+    def post(self, request, pk):
+        article = get_object_or_404(Article, pk=pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if not serializer.is_valid():
+            return Response({'serializer': serializer, 'article': article})
+        serializer.save()
+        return redirect('ArticleList')
