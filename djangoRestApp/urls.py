935fe306d11732c01355b6acd4577e9681a707ff
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-04-21 21:33:48 +0600

    Generic API VIews & Mixins

--- djangoRestApp/urls.py
+++ djangoRestApp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     # Class Based Urls
     path('', views.Articlelist.as_view(), name='articlelist'),
     path('<int:pk>/', views.Articledetails.as_view(), name='articledetails'),
+    path('generic/', views.GenericArticleView.as_view(), name='GenericArticleView'),
+    path('generic/<int:id>', views.GenericArticleView.as_view(), name='GenericArticleView'),
 ]
--- djangoRestApp/views.py
+++ djangoRestApp/views.py
@@ -5,6 +5,30 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from rest_framework import generics
+from rest_framework import mixins
+# Generic views & Mixins
+class GenericArticleView(generics.GenericAPIView, mixins.ListModelMixin,
+                         mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                         mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    lookup_field = 'id'
+    
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request, id)
+        else:
+            return self.list(request)
+    def post(self, request):
+        return self.create(request)
+    def put(self, request, id=None):
+        return self.update(request, id)
+    def delete(self, request, id=None):
+        return self.destroy(request, id)
+    
+
+
 # Model Based Views
 class Articlelist(APIView):
 	def get(self, request):
