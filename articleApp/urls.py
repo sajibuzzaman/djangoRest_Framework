2ab5dd0c880a79857f48ae71cd6e4a8491fc7bc6
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-04-21 18:03:45 +0600

    create articleApp

---
+++ articleApp/urls.py
@@ -0,0 +1,6 @@
+from django.conf import urls
+from django.urls import path
+
+urlpatterns = [
+    
+]
--- djangoRest/settings.py
+++ djangoRest/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # apps
     'djangoRestApp',
+    'articleApp',
 
     # Rest Framework
     'rest_framework',
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'djangoRest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    BASE_DIR / 'static',
+]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
